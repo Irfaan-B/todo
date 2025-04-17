592ce7a61b0507bd489a5d340ff6e83c626c39d4
Irfaan Bodha <irfaan.bodha@gmail.com>
2025-04-17 21:58:58 +0200

    gitignore

--- main.cpp
+++ main.cpp
@@ -4,13 +4,8 @@
 
 using namespace std;
 
+
 int main() {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-    
-    for (const string& word : msg) {
-        cout << word << " ";
-    }
-    cout << endl;
     
     return 0;
 }
